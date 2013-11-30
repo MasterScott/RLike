b4edca073e5e4ba7f06b899981ac877d9aa8724b
Dan Watland <danwatland@gmail.com>
2013-11-30 08:18:11 -0600

    Did additional work on character selection screen.

---
+++ RLike/src/roguelike/main/ClassSelection.java
@@ -0,0 +1,15 @@
+package roguelike.main;
+
+import roguelike.ui.CharacterSelectPanel;
+import roguelike.ui.Window;
+
+public class ClassSelection {
+
+	public static void main(String[] args) {
+		Window w = new Window(0, 0, 800, 600);
+		CharacterSelectPanel p = new CharacterSelectPanel();
+		w.add(p);
+		//w.pack();
+		w.setVisible(true);
+	}
+}
--- RLike/src/roguelike/ui/CharacterSelectPanel.java
+++ RLike/src/roguelike/ui/CharacterSelectPanel.java
@@ -11,8 +11,11 @@ import javax.swing.SwingConstants;
 import net.miginfocom.swing.MigLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
 
 public class CharacterSelectPanel extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
@@ -21,90 +24,99 @@ public class CharacterSelectPanel extends JPanel {
 		setForeground(Color.WHITE);
 		//setBounds(0, 0, getParent().getWidth(), getParent().getHeight());
 		setBackground(Color.BLACK);
-		setLayout(new MigLayout("", "[15.56%][1.01%][30.10%][1.01%][12.16%][30.82%]", "[7.44%][][][][][][][][][][][][][][]"));
+		setLayout(new MigLayout("", "[15.56%][1.01%][32.56%][1.01%][12.16%][31.98%]", "[][7.44%][][][][][][][][][][][][][][]"));
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblName.setForeground(Color.WHITE);
+		add(lblName, "cell 0 0,alignx center");
+		
+		textField = new JTextField();
+		add(textField, "cell 2 0,growx");
+		textField.setColumns(10);
 		
 		JLabel lblRace = new JLabel("Race");
 		lblRace.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblRace.setForeground(Color.WHITE);
-		add(lblRace, "cell 0 0,alignx center");
+		add(lblRace, "flowy,cell 0 1,alignx center");
 		
 		JLabel lblNewLabel_8 = new JLabel("Words go here");
 		lblNewLabel_8.setForeground(Color.WHITE);
-		add(lblNewLabel_8, "cell 2 0 1 9,alignx center,aligny center");
+		add(lblNewLabel_8, "cell 2 1 1 9,alignx center,aligny center");
 		
 		JLabel lblClass = new JLabel("Class");
 		lblClass.setForeground(Color.WHITE);
 		lblClass.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		add(lblClass, "cell 4 0,alignx center");
+		add(lblClass, "flowy,cell 4 1,alignx center");
 		
 		JLabel lblNewLabel_17 = new JLabel("Words go here");
 		lblNewLabel_17.setForeground(Color.WHITE);
-		add(lblNewLabel_17, "cell 5 0 1 9,alignx center,aligny center");
+		add(lblNewLabel_17, "cell 5 1 1 9,alignx center,aligny center");
 		
-		JLabel lblNewLabel = new JLabel("Buff Human");
-		lblNewLabel.setForeground(Color.WHITE);
-		add(lblNewLabel, "cell 0 1,alignx center");
+		JLabel lblRace0 = new JLabel("Buff Human");
+		lblRace0.setForeground(Color.WHITE);
+		add(lblRace0, "cell 0 2,alignx center");
 		
-		JLabel lblNewLabel_9 = new JLabel("Mage");
-		lblNewLabel_9.setForeground(Color.WHITE);
-		add(lblNewLabel_9, "cell 4 1,alignx center");
+		JLabel lblClass0 = new JLabel("Mage");
+		lblClass0.setForeground(Color.WHITE);
+		add(lblClass0, "cell 4 2,alignx center");
 		
-		JLabel lblNewLabel_1 = new JLabel("Human");
-		lblNewLabel_1.setForeground(Color.WHITE);
-		add(lblNewLabel_1, "cell 0 2,alignx center");
+		JLabel lblRace1 = new JLabel("Human");
+		lblRace1.setForeground(Color.WHITE);
+		add(lblRace1, "cell 0 3,alignx center");
 		
-		JLabel lblNewLabel_10 = new JLabel("Wizard");
-		lblNewLabel_10.setForeground(Color.WHITE);
-		add(lblNewLabel_10, "cell 4 2,alignx center");
+		JLabel lblClass2 = new JLabel("Wizard");
+		lblClass2.setForeground(Color.WHITE);
+		add(lblClass2, "cell 4 3,alignx center");
 		
-		JLabel lblNewLabel_2 = new JLabel("Blond Hair Dude");
-		lblNewLabel_2.setForeground(Color.WHITE);
-		add(lblNewLabel_2, "cell 0 3,alignx center");
+		JLabel lblRace2 = new JLabel("Blond Hair Dude");
+		lblRace2.setForeground(Color.WHITE);
+		add(lblRace2, "cell 0 4,alignx center");
 		
-		JLabel lblNewLabel_11 = new JLabel("Monk");
-		lblNewLabel_11.setForeground(Color.WHITE);
-		add(lblNewLabel_11, "cell 4 3,alignx center");
+		JLabel lblClass3 = new JLabel("Monk");
+		lblClass3.setForeground(Color.WHITE);
+		add(lblClass3, "cell 4 4,alignx center");
 		
-		JLabel lblNewLabel_3 = new JLabel("Hobbit");
-		lblNewLabel_3.setForeground(Color.WHITE);
-		add(lblNewLabel_3, "cell 0 4,alignx center");
+		JLabel lblRace3 = new JLabel("Hobbit");
+		lblRace3.setForeground(Color.WHITE);
+		add(lblRace3, "cell 0 5,alignx center");
 		
-		JLabel lblNewLabel_12 = new JLabel("Knight");
-		lblNewLabel_12.setForeground(Color.WHITE);
-		add(lblNewLabel_12, "cell 4 4,alignx center");
+		JLabel lblClass4 = new JLabel("Knight");
+		lblClass4.setForeground(Color.WHITE);
+		add(lblClass4, "cell 4 5,alignx center");
 		
-		JLabel lblNewLabel_4 = new JLabel("Dwarf");
-		lblNewLabel_4.setForeground(Color.WHITE);
-		add(lblNewLabel_4, "cell 0 5,alignx center");
+		JLabel lblRace4 = new JLabel("Ginger");
+		lblRace4.setForeground(Color.WHITE);
+		add(lblRace4, "cell 0 6,alignx center");
 		
-		JLabel lblNewLabel_13 = new JLabel("Priest");
-		lblNewLabel_13.setForeground(Color.WHITE);
-		add(lblNewLabel_13, "cell 4 5,alignx center");
+		JLabel lblClass5 = new JLabel("Priest");
+		lblClass5.setForeground(Color.WHITE);
+		add(lblClass5, "cell 4 6,alignx center");
 		
-		JLabel lblNewLabel_5 = new JLabel("Blue Guy");
-		lblNewLabel_5.setForeground(Color.WHITE);
-		add(lblNewLabel_5, "cell 0 6,alignx center");
+		JLabel lblRace5 = new JLabel("Dwarf");
+		lblRace5.setForeground(Color.WHITE);
+		add(lblRace5, "cell 0 7,alignx center");
 		
-		JLabel lblNewLabel_14 = new JLabel("Fighter");
-		lblNewLabel_14.setForeground(Color.WHITE);
-		add(lblNewLabel_14, "cell 4 6,alignx center");
+		JLabel lblClass6 = new JLabel("Fighter");
+		lblClass6.setForeground(Color.WHITE);
+		add(lblClass6, "cell 4 7,alignx center");
 		
-		JLabel lblNewLabel_6 = new JLabel("Dark Skin Mans");
-		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_6.setForeground(Color.WHITE);
-		add(lblNewLabel_6, "cell 0 7,alignx center");
+		JLabel lblRace6 = new JLabel("Blue Guy");
+		lblRace6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRace6.setForeground(Color.WHITE);
+		add(lblRace6, "cell 0 8,alignx center");
 		
-		JLabel lblNewLabel_15 = new JLabel("Thief");
-		lblNewLabel_15.setForeground(Color.WHITE);
-		add(lblNewLabel_15, "cell 4 7,alignx center");
+		JLabel lblClass7 = new JLabel("Thief");
+		lblClass7.setForeground(Color.WHITE);
+		add(lblClass7, "cell 4 8,alignx center");
 		
-		JLabel lblNewLabel_7 = new JLabel("Green Guy");
-		lblNewLabel_7.setForeground(Color.WHITE);
-		add(lblNewLabel_7, "cell 0 8,alignx center");
+		JLabel lblRace7 = new JLabel("Golem");
+		lblRace7.setForeground(Color.WHITE);
+		add(lblRace7, "cell 0 9,alignx center");
 		
-		JLabel lblNewLabel_16 = new JLabel("Gladiator");
-		lblNewLabel_16.setForeground(Color.WHITE);
-		add(lblNewLabel_16, "cell 4 8,alignx center");
+		JLabel lblClass8 = new JLabel("Gladiator");
+		lblClass8.setForeground(Color.WHITE);
+		add(lblClass8, "cell 4 9,alignx center");
 		
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addMouseListener(new MouseAdapter() {
@@ -112,7 +124,19 @@ public class CharacterSelectPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		add(btnCancel, "flowx,cell 5 14,alignx center");
+		
+		JLabel lblRace8 = new JLabel("Halfling");
+		lblRace8.setForeground(Color.WHITE);
+		add(lblRace8, "cell 0 10,alignx center");
+		
+		JLabel lblRace9 = new JLabel("Dark Skin Mans");
+		lblRace9.setForeground(Color.WHITE);
+		add(lblRace9, "cell 0 11,alignx center");
+		
+		JLabel lblRace10 = new JLabel("Green Guy");
+		lblRace10.setForeground(Color.WHITE);
+		add(lblRace10, "cell 0 12,alignx center");
+		add(btnCancel, "flowx,cell 5 15,alignx center");
 		
 		JButton btnOk = new JButton("OK");
 		btnOk.addMouseListener(new MouseAdapter() {
@@ -120,7 +144,13 @@ public class CharacterSelectPanel extends JPanel {
 			public void mouseClicked(MouseEvent arg0) {
 			}
 		});
-		add(btnOk, "cell 5 14,alignx center");
+		add(btnOk, "cell 5 15,alignx center");
+		
+		JSeparator separator = new JSeparator();
+		add(separator, "cell 0 1,growx");
+		
+		JSeparator separator_1 = new JSeparator();
+		add(separator_1, "cell 4 1,growx");
 		
 		
 	}
