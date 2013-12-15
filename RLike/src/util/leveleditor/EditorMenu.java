9ba632b59372e8e7a8b598b500fead45664f1739
Dan Watland <danwatland@gmail.com>
2013-12-15 09:39:45 -0600

    Added selected tile to menu display.

--- RLike/src/util/leveleditor/EditorMenu.java
+++ RLike/src/util/leveleditor/EditorMenu.java
@@ -25,13 +25,14 @@ import javax.swing.ListSelectionModel;
 import net.miginfocom.swing.MigLayout;
 import roguelike.ui.graphics.Graphic;
 import roguelike.ui.graphics.Graphic.GraphicFile;
+import javax.swing.SwingConstants;
 
 public class EditorMenu extends JPanel {
 
 	private static final long serialVersionUID = -5323835129443241111L;
 	private JTextField textFieldWidth;
 	private JTextField textFieldHeight;
-	private JLabel lblX, lblY, lblTileset;
+	private JLabel lblX, lblY, lblTileset, lblTile;
 	private EditorWindow parent;
 	int x, y;
 
@@ -39,7 +40,7 @@ public class EditorMenu extends JPanel {
 	 * Create the panel.
 	 */
 	public EditorMenu() {
-		setLayout(new MigLayout("", "[]", "[18.00][28.00][][][][][][][]"));
+		setLayout(new MigLayout("", "[][]", "[18.00][28.00][][][][][][][]"));
 		setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
 		ArrayList<String> arr = new ArrayList<String>();
@@ -48,23 +49,23 @@ public class EditorMenu extends JPanel {
 		}
 
 		JLabel lblLevelEditor = new JLabel("Level Editor");
-		add(lblLevelEditor, "cell 0 0");
+		add(lblLevelEditor, "cell 0 0 2 1");
 
 		JButton btnNew = new JButton("New");
-		add(btnNew, "flowx,cell 0 1");
+		add(btnNew, "flowx,cell 0 1 2 1");
 
 		JButton btnOpen = new JButton("Open");
-		add(btnOpen, "cell 0 1");
+		add(btnOpen, "cell 0 1 2 1");
 
 		JButton btnSave = new JButton("Save");
-		add(btnSave, "cell 0 1");
+		add(btnSave, "cell 0 1 2 1");
 
 		JButton btnSaveAs = new JButton("Save As");
-		add(btnSaveAs, "cell 0 1");
+		add(btnSaveAs, "cell 0 1 2 1");
 
 		JSeparator separator = new JSeparator();
 		separator.setForeground(Color.BLACK);
-		add(separator, "cell 0 2,growx");
+		add(separator, "cell 0 2 2 1,growx");
 
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		final JList list = new JList(arr.toArray());
@@ -80,33 +81,44 @@ public class EditorMenu extends JPanel {
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setVisibleRowCount(-1);
-		add(list, "cell 0 4,alignx left");
+		add(list, "flowx,cell 0 4,alignx left");
 
 		JLabel lblWidth = new JLabel("Width: ");
-		add(lblWidth, "flowx,cell 0 3");
+		add(lblWidth, "flowx,cell 0 3 2 1");
 
 		textFieldWidth = new JTextField();
 		textFieldWidth.setPreferredSize(new Dimension(24, 24));
-		add(textFieldWidth, "cell 0 3");
+		add(textFieldWidth, "cell 0 3 2 1");
 		textFieldWidth.setColumns(3);
 
 		JLabel lblHeight = new JLabel("Height:");
-		add(lblHeight, "cell 0 3");
+		add(lblHeight, "cell 0 3 2 1");
 
 		textFieldHeight = new JTextField();
 		textFieldHeight.setPreferredSize(new Dimension(24, 24));
-		add(textFieldHeight, "cell 0 3");
+		add(textFieldHeight, "cell 0 3 2 1");
 		textFieldHeight.setColumns(3);
 
 		lblTileset = new JLabel();
 		Dimension d = new Dimension(250, 250);
-		add(lblTileset, "cell 0 5");
+		
+		Box verticalBox = Box.createVerticalBox();
+		add(verticalBox, "flowx,cell 1 4,alignx center,aligny top");
+		
+		JLabel lblSelectedTile = new JLabel("Selected Tile:");
+		lblSelectedTile.setHorizontalAlignment(SwingConstants.CENTER);
+		verticalBox.add(lblSelectedTile);
+		
+		lblTile = new JLabel();
+		lblTile.setHorizontalAlignment(SwingConstants.CENTER);
+		verticalBox.add(lblTile);
+		add(lblTileset, "cell 0 5 2 1");
 
 		// $hide>>$
 		final JScrollPane tilesetScroller = new JScrollPane(lblTileset);
 		tilesetScroller.setPreferredSize(d);
 		tilesetScroller.setMinimumSize(d);
-		add(tilesetScroller, "cell 0 5");
+		add(tilesetScroller, "cell 0 5 2 1");
 
 		tilesetScroller.addMouseListener(new MouseListener() {
 
@@ -136,6 +148,7 @@ public class EditorMenu extends JPanel {
 					int dx = tilesetScroller.getHorizontalScrollBar().getValue();
 					int dy = tilesetScroller.getVerticalScrollBar().getValue();
 					Image img = Graphic.getImage(GraphicFile.valueOf(selection), (y + dy) / 32, (x + dx) / 32);
+					lblTile.setIcon(new ImageIcon(img));
 					parent.setSelectedImage(img);
 				}
 
@@ -144,13 +157,15 @@ public class EditorMenu extends JPanel {
 		// $hide<<$
 
 		lblX = new JLabel("X: ");
-		add(lblX, "flowx,cell 0 8");
+		add(lblX, "flowx,cell 0 8 2 1");
 
 		Component horizontalStrut = Box.createHorizontalStrut(20);
-		add(horizontalStrut, "cell 0 8");
+		add(horizontalStrut, "cell 0 8 2 1");
 
 		lblY = new JLabel("Y: ");
-		add(lblY, "cell 0 8");
+		add(lblY, "cell 0 8 2 1");
+		
+		
 
 		// WindowBuilder can't parse this.
 		// $hide>>$
