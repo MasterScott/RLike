ef29d0ce310f0db1478665bf0811cca19498ce66
Dan Watland <danwatland@gmail.com>
2014-01-07 08:44:47 -0600

    Focus now transfers correctly between inventory and game.

--- RLike/src/roguelike/main/LoadInventory.java
+++ RLike/src/roguelike/main/LoadInventory.java
@@ -1,64 +1,55 @@
 package roguelike.main;
 
-import java.util.Enumeration;
+import java.awt.Color;
+import java.awt.Point;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.UIDefaults;
-import javax.swing.UIManager;
+import javax.swing.JDesktopPane;
+
+import roguelike.actors.Player;
+import roguelike.etc.Session;
+import roguelike.ui.ActionPanel;
+import roguelike.ui.InventoryFrame;
+import roguelike.ui.StatsPanel;
+import roguelike.ui.Window;
+import roguelike.ui.graphics.Graphic.GraphicFile;
+import roguelike.world.BaseDungeon;
 
 public class LoadInventory {
 
 	public static void main(String[] args) {
-		
-		UIDefaults defaults = UIManager.getDefaults();
-        System.out.println(defaults.size()+ " properties deffined !");
-        String[ ] colName = {"Key", "Value"};
-        String[ ][ ] rowData = new String[ defaults.size() ][ 2 ];
-        int i = 0;
-        for(Enumeration e = defaults.keys(); e.hasMoreElements(); i++){
-            Object key = e.nextElement();
-            rowData[ i ] [ 0 ] = key.toString();
-            rowData[ i ] [ 1 ] = ""+defaults.get(key);
-            System.out.println(rowData[i][0]+" ,, "+rowData[i][1]);
-        }
-        JFrame f = new JFrame("UIManager properties default values");
-        JTable t = new JTable(rowData, colName);
-        f.setContentPane(new JScrollPane(t));
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.pack();
-        f.setVisible(true);
         
-//      Window w = new Window();
-//      JDesktopPane jdp = new JDesktopPane();
-//		
-//		w.add(jdp);
-//		Player p = new Player(20, 10);
-//		Session.player = p;
-//		p.setImage(GraphicFile.CLASSM, 2, 5);
-//		
-//		BaseDungeon dungeon = new BaseDungeon();
-//		dungeon.generateFloor();
-//		dungeon.actors.add(p);
-//		p.setFloor(dungeon);
-//		Point point = dungeon.getRandomOpenTile();
-//		p.setCoords(point.x, point.y);
-//		
-//		StatsPanel sp = new StatsPanel();
-//		sp.setPreferredSize(sp.getSize());
-//		sp.setMaximumSize(sp.getSize());
-//		ActionPanel ap = new ActionPanel();
-//		ap.setBackground(Color.BLACK);
-//		jdp.add(ap);
-//		jdp.add(sp);
-//		ap.setFloor(dungeon);
-//		
-//		w.setVisible(true);
-//		
-//		InventoryFrame ip = new InventoryFrame();
-//		jdp.add(ip);
-//		ip.setVisible(true);
+		Window w = new Window();
+		JDesktopPane jdp = new JDesktopPane();
+
+		w.add(jdp);
+		Player p = new Player(20, 10);
+		Session.player = p;
+		p.setImage(GraphicFile.CLASSM, 2, 5);
+
+		BaseDungeon dungeon = new BaseDungeon();
+		dungeon.generateFloor();
+		dungeon.actors.add(p);
+		p.setFloor(dungeon);
+		Point point = dungeon.getRandomOpenTile();
+		p.setCoords(point.x, point.y);
+
+		StatsPanel sp = new StatsPanel();
+		sp.setPreferredSize(sp.getSize());
+		sp.setMaximumSize(sp.getSize());
+		ActionPanel ap = new ActionPanel();
+		ap.setBackground(Color.BLACK);
+		jdp.add(ap);
+		jdp.add(sp);
+		ap.setFloor(dungeon);
+
+		w.setVisible(true);
+		ap.requestFocusInWindow();
+
+		InventoryFrame ip = new InventoryFrame();
+		ip.ap = ap;
+		jdp.add(ip);
+		ip.setVisible(true);
+		
 		
 	}
 }
--- RLike/src/roguelike/ui/InventoryFrame.java
+++ RLike/src/roguelike/ui/InventoryFrame.java
@@ -4,14 +4,19 @@ import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
-import javax.swing.UIManager;
 import javax.swing.border.EtchedBorder;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 import net.miginfocom.swing.MigLayout;
 
 public class InventoryFrame extends JInternalFrame {
 
+	private static final long serialVersionUID = -1745769096143603577L;
+	// TODO Change how this works.
+	public ActionPanel ap;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -34,12 +39,20 @@ public class InventoryFrame extends JInternalFrame {
 	public InventoryFrame() {
 		super("Inventory", false, true);
 		getContentPane().setBackground(Color.BLACK);
-		setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.LIGHT_GRAY, null));
+		setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.LIGHT_GRAY, Color.WHITE));
 		
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new MigLayout("", "[]", "[]"));
 		setUI(new BasicInternalFrameUI(this));
-		UIManager.put("JInternalFrame.activeTitleBackground", Color.red);
+		
+		addInternalFrameListener(new InternalFrameAdapter() {
+			@Override
+			public void internalFrameClosed(InternalFrameEvent e) {
+				ap.requestFocusInWindow();
+			}
+		});
 	}
+	
+	
 
 }
