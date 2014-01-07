7e6d538862b5d3e147334e0414542f6c760d21f8
Dan Watland <danwatland@gmail.com>
2014-01-07 06:17:59 -0600

    More work on inventory window.

--- RLike/src/roguelike/main/LoadInventory.java
+++ RLike/src/roguelike/main/LoadInventory.java
@@ -1,50 +1,64 @@
 package roguelike.main;
 
-import java.awt.Color;
-import java.awt.Point;
+import java.util.Enumeration;
 
-import javax.swing.JDesktopPane;
-
-import roguelike.actors.Player;
-import roguelike.etc.Session;
-import roguelike.ui.ActionPanel;
-import roguelike.ui.InventoryFrame;
-import roguelike.ui.StatsPanel;
-import roguelike.ui.Window;
-import roguelike.ui.graphics.Graphic.GraphicFile;
-import roguelike.world.BaseDungeon;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
 
 public class LoadInventory {
 
 	public static void main(String[] args) {
-Window w = new Window();
-JDesktopPane jdp = new JDesktopPane();
 		
-		w.add(jdp);
-		Player p = new Player(20, 10);
-		Session.player = p;
-		p.setImage(GraphicFile.CLASSM, 2, 5);
+		UIDefaults defaults = UIManager.getDefaults();
+        System.out.println(defaults.size()+ " properties deffined !");
+        String[ ] colName = {"Key", "Value"};
+        String[ ][ ] rowData = new String[ defaults.size() ][ 2 ];
+        int i = 0;
+        for(Enumeration e = defaults.keys(); e.hasMoreElements(); i++){
+            Object key = e.nextElement();
+            rowData[ i ] [ 0 ] = key.toString();
+            rowData[ i ] [ 1 ] = ""+defaults.get(key);
+            System.out.println(rowData[i][0]+" ,, "+rowData[i][1]);
+        }
+        JFrame f = new JFrame("UIManager properties default values");
+        JTable t = new JTable(rowData, colName);
+        f.setContentPane(new JScrollPane(t));
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);
+        
+//      Window w = new Window();
+//      JDesktopPane jdp = new JDesktopPane();
+//		
+//		w.add(jdp);
+//		Player p = new Player(20, 10);
+//		Session.player = p;
+//		p.setImage(GraphicFile.CLASSM, 2, 5);
+//		
+//		BaseDungeon dungeon = new BaseDungeon();
+//		dungeon.generateFloor();
+//		dungeon.actors.add(p);
+//		p.setFloor(dungeon);
+//		Point point = dungeon.getRandomOpenTile();
+//		p.setCoords(point.x, point.y);
+//		
+//		StatsPanel sp = new StatsPanel();
+//		sp.setPreferredSize(sp.getSize());
+//		sp.setMaximumSize(sp.getSize());
+//		ActionPanel ap = new ActionPanel();
+//		ap.setBackground(Color.BLACK);
+//		jdp.add(ap);
+//		jdp.add(sp);
+//		ap.setFloor(dungeon);
+//		
+//		w.setVisible(true);
+//		
+//		InventoryFrame ip = new InventoryFrame();
+//		jdp.add(ip);
+//		ip.setVisible(true);
 		
-		BaseDungeon dungeon = new BaseDungeon();
-		dungeon.generateFloor();
-		dungeon.actors.add(p);
-		p.setFloor(dungeon);
-		Point point = dungeon.getRandomOpenTile();
-		p.setCoords(point.x, point.y);
-		
-		StatsPanel sp = new StatsPanel();
-		sp.setPreferredSize(sp.getSize());
-		sp.setMaximumSize(sp.getSize());
-		ActionPanel ap = new ActionPanel();
-		ap.setBackground(Color.BLACK);
-		jdp.add(ap);
-		jdp.add(sp);
-		ap.setFloor(dungeon);
-		
-		w.setVisible(true);
-		
-		InventoryFrame ip = new InventoryFrame();
-		jdp.add(ip);
-		ip.setVisible(true);
 	}
 }
--- RLike/src/roguelike/ui/InventoryFrame.java
+++ RLike/src/roguelike/ui/InventoryFrame.java
@@ -1,8 +1,13 @@
 package roguelike.ui;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
 import net.miginfocom.swing.MigLayout;
 
 public class InventoryFrame extends JInternalFrame {
@@ -27,9 +32,14 @@ public class InventoryFrame extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public InventoryFrame() {
+		super("Inventory", false, true);
+		getContentPane().setBackground(Color.BLACK);
+		setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.LIGHT_GRAY, null));
+		
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new MigLayout("", "[]", "[]"));
-
+		setUI(new BasicInternalFrameUI(this));
+		UIManager.put("JInternalFrame.activeTitleBackground", Color.red);
 	}
 
 }
